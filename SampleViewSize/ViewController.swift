2d92c5c9560d378374864b5bf74a5f6b0ec5db94
Arthit Thongpan <arthit.tp@me.com>
2015-09-04 01:10:12 +0700

    Initial Commit

---
+++ SampleViewSize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleViewSize
+//
+//  Created by Arthit Thongpan on 9/4/2558 BE.
+//  Copyright (c) 2558 Arthit Thongpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
